e5b02cedc230309c126fd7ffbaa55a76505d6a6c
xArkqngel <arkqngel343@gmail.com>
2022-11-19 18:11:49 -0500

    showing data in table from json

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-print("Hello World");
+const listUsers= async ()=>{
+        const response= await fetch('https://jsonplaceholder.typicode.com/users');
+        const cows = await response.json();
+
+        let tableBody = ``;
+        
+        cows.forEach((cow, index)=>{
+                tableBody += `<tr>
+                        <td>${index+1}</td>
+                        <td>${cow.name}</td>
+                        <td>${cow.email}</td>
+                        <td>${cow.phone}</td>
+                        <td>${cow.website}</td>
+                </tr>`
+        });
+        //document.getElementById('tableBody_Cows').innerHTML = tableBody;
+        tableBody_Cows.innerHTML = tableBody;
+}
+
+window.addEventListener('load', function() {
+        listUsers();
+});

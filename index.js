e510f6fb7cae6464cea62f6228365c98220f496e
xArkqngel <arkqngel343@gmail.com>
2022-11-24 14:21:41 -0500

    ICOW-39 | Panel Landpage (#12)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-print("Hello World");
+const listUsers= async ()=>{
+        const response= await fetch('https://jsonplaceholder.typicode.com/users');
+        const cows = await response.json();
+
+        let tableBody = ``;
+        
+        cows.forEach((cow, index)=>{
+                tableBody += `<tr>
+                        <td>${index+1}</td>
+                        <td>${cow.name}</td>
+                        <td>${cow.email}</td>
+                        <td>${cow.phone}</td>
+                        <td>${cow.website}</td>
+                        <td></td>
+                        <td></td>
+                </tr>`
+        });
+        //document.getElementById('tableBody_Cows').innerHTML = tableBody;
+        tableBody_Cows.innerHTML = tableBody;
+}
+
+window.addEventListener('load', function() {
+        listUsers();
+});

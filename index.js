a02d3bedcd481853cd0a9763da4af5b34cea31f2
xArkqngel <arkqngel343@gmail.com>
2022-12-04 09:09:06 -0500

    ICOW-54 | Panel fincas (#16)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const listUsers= async ()=>{
+const listCows= async ()=>{
 	const response= await fetch('https://jsonplaceholder.typicode.com/users');
 	const cows = await response.json();
 
@@ -7,24 +7,75 @@ const listUsers= async ()=>{
 	cows.forEach((cow, index)=>{
 		tableBody += `<tr>
                         <td>${index+1}</td>
+                        <td>${cow.id}</td>
                         <td>${cow.name}</td>
                         <td>${cow.email}</td>
                         <td>${cow.phone}</td>
                         <td>${cow.website}</td>
                         <td></td>
                         <td>
-                                <button class="btn btn-m btn-primary"><i class="fa-regular fa-pen-to-square"></i></button>
-                                <button class="btn btn-m btn-danger"><i class="fa-solid fa-trash-can"></i></i></button>
+                                <button class="btn btn-m btn-primary" title="Editar"><i class="fa-regular fa-pen-to-square"></i></button>
+                                <button class="btn btn-m btn-danger" title="Eliminar"><i class="fa-solid fa-trash-can"></i></i></button>
                         </td>
                 </tr>`;
 	});
-	//document.getElementById('tableBody_Cows').innerHTML = tableBody;
 	// eslint-disable-next-line no-undef
 	tableBody_Cows.innerHTML = tableBody;
 };
 
+const listFincas= async ()=>{
+	const response= await fetch('https://jsonplaceholder.typicode.com/users');
+	const fincas = await response.json();
+
+	let tableBody = '';
+        
+	fincas.forEach((finca, index)=>{
+		tableBody += `<tr>
+						<td>${index+1}</td>
+                        <td>${finca.id}</td>
+                        <td>${finca.name}</td>
+                        <td>${finca.address.street}</td>
+                        <td>${finca.phone}</td>
+                        <td>
+                                <button class="btn btn-m btn-primary" title="Editar"><i class="fa-regular fa-pen-to-square"></i></button>
+                                <button class="btn btn-m btn-danger" title="Eliminar"><i class="fa-solid fa-trash-can"></i></button>
+                                <button class="btn btn-m btn-info" title="Ver lotes"><i class="fa-regular fa-eye"></i></button>
+                        </td>
+                </tr>`;
+	});
+	// eslint-disable-next-line no-undef
+	tableBody_Fincas.innerHTML = tableBody;
+};
+
+const listUsers= async ()=>{
+	const response= await fetch('https://jsonplaceholder.typicode.com/users');
+	const users = await response.json();
+
+	let tableBody = '';
+        
+	users.forEach((user, index)=>{
+		tableBody += `<tr>
+						<td>${index+1}</td>
+                        <td>${user.id}</td>
+                        <td>${user.username}</td>
+                        <td>${user.name}</td>
+                        <td>${user.name}</td> <!-- lastname -->
+                        <td>${user.email}</td>
+                        <td></td> <!-- role -->
+                        <td>
+                                <button class="btn btn-m btn-primary" title="Editar"><i class="fa-regular fa-pen-to-square"></i></button>
+                                <button class="btn btn-m btn-danger" title="Eliminar"><i class="fa-solid fa-trash-can"></i></button>
+                        </td>
+                </tr>`;
+	});
+	// eslint-disable-next-line no-undef
+	tableBody_Users.innerHTML = tableBody;
+};
+
 window.addEventListener('load', function() {
+	listCows();
 	listUsers();
+	listFincas();
 });
 
 const hidePadres = document.getElementById('hidePadres');

a51807ebfe9bad342599caa4b8cb96d585252e11
xArkqngel <arkqngel343@gmail.com>
2022-12-09 11:36:16 -0500

    changed js table structure

--- index.js
+++ index.js
@@ -48,28 +48,37 @@ const listFincas= async ()=>{
 };
 
 const listUsers= async ()=>{
-	const response= await fetch('https://jsonplaceholder.typicode.com/users');
+	const response= await fetch('http://127.0.0.1:3001/persons');
 	const users = await response.json();
 
-	let tableBody = '';
-        
-	users.forEach((user, index)=>{
-		tableBody += `<tr>
-						<td>${index+1}</td>
-                        <td>${user.id}</td>
-                        <td>${user.username}</td>
-                        <td>${user.name}</td>
-                        <td>${user.name}</td> <!-- lastname -->
-                        <td>${user.email}</td>
-                        <td></td> <!-- role -->
-                        <td>
-                                <button class="btn btn-m btn-primary" title="Editar"><i class="fa-regular fa-pen-to-square"></i></button>
-                                <button class="btn btn-m btn-danger" title="Eliminar"><i class="fa-solid fa-trash-can"></i></button>
-                        </td>
-                </tr>`;
+	const finalUsers = users.persons;
+	console.log(finalUsers);
+	finalUsers.forEach((user, index)=>{
+		var tdName= document.createElement('td');
+		var tdIndex= document.createElement('td');
+		var tdLastName= document.createElement('td');
+		var tdEmail= document.createElement('td');
+		var tdAdmin= document.createElement('td');
+		var tdActions= document.createElement('td');
+		var tr= document.createElement('tr');
+		if(user.is_admin === true){
+			tdAdmin.innerHTML = '<i class="fa-solid fa-user-check" text="success"></i>';
+		}else{
+			tdAdmin.innerHTML = '<i class="fa-solid fa-user-large-slash" style"color:red"></i>';
+		}
+		tdIndex.innerHTML = index+1;
+		tdName.innerHTML = user.name;
+		tdLastName.innerHTML = user.last_name;
+		tdEmail.innerHTML = user.email;
+		tdActions.innerHTML = '<button class="btn btn-m btn-primary" title="Editar"><i class="fa-regular fa-pen-to-square"></i></button><button class="btn btn-m btn-danger" title="Eliminar"><i class="fa-solid fa-trash-can"></i></button>';
+		tr.appendChild(tdIndex);
+		tr.appendChild(tdName);
+		tr.appendChild(tdLastName);
+		tr.appendChild(tdEmail);
+		tr.appendChild(tdAdmin);
+		tr.appendChild(tdActions);
+		document.getElementById('tableBody_Users').appendChild(tr);
 	});
-	// eslint-disable-next-line no-undef
-	tableBody_Users.innerHTML = tableBody;
 };
 
 const listLotes= async ()=>{
